ddc5d77d57705195451f472162d5714c3c5a93cd
testeredekiadam <erkolfirat@gmail.com>
2022-05-03 04:12:28 +0200

    Lombok Slf4j <-> Logger

--- console/src/main/java/academy/learnprogramming/console/Main.java
+++ console/src/main/java/academy/learnprogramming/console/Main.java
@@ -1,14 +1,13 @@
 package academy.learnprogramming.console;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import academy.learnprogramming.config.GameConfig;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class Main {
-	private static final Logger log = LoggerFactory.getLogger(Main.class);
 	
 	public static void main(String[] args) {
 		log.info("Guess The Number Game");
--- core/src/main/java/academy/learnprogramming/NumberGeneratorImpl.java
+++ core/src/main/java/academy/learnprogramming/NumberGeneratorImpl.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+
 @Component
 public class NumberGeneratorImpl implements NumberGenerator {
 
